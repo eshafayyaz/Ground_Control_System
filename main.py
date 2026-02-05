f70e441eb22ba7b208657037ea75ffb07a3fcc29
eshafayyazz <eshafayyaz76@gmail.com>
2026-02-05 17:49:16 +0500

    update

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from presentation.menu import Menu
-menu = Menu()
-choice = menu.show_menu()
+from services.mission_service import MissionService
+
+# objects
+mission_service = MissionService()   # MissionService ka object bana
+menu = Menu(mission_service)         # Menu ka object bana aur MissionService ka object pass kiya
+menu.display_menu()                  # Menu display karo
