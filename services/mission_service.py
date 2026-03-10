9d61dea3e8aba183491e2f5c8c8c7a8756655f56
eshafayyazz <eshafayyaz76@gmail.com>
2026-03-10 13:59:12 +0500

    update

--- services/mission_service.py
+++ services/mission_service.py
@@ -6,6 +6,16 @@ class MissionService:
     def __init__(self):
         self.missions: List[Mission] = []
 
+    def create_mission(self, mission_id: int, name: str, description: str) -> Mission:
+        existing_mission = self.get_mission(mission_id)
+
+        if existing_mission is not None:
+            raise ValueError("Mission already exists with this ID")
+
+        new_mission = Mission(mission_id, name, description)
+        self.missions.append(new_mission)
+        return new_mission
+
     def get_mission(self, mission_id: int) -> Mission | None:
         for mission in self.missions:
             if mission.mission_id == mission_id:
@@ -14,12 +24,3 @@ class MissionService:
 
     def get_all_missions(self) -> List[Mission]:
         return self.missions
-
-    def create_mission(self, mission_id: int, name: str, description: str) -> None:
-        existing_mission = self.get_mission(mission_id)
-
-        if existing_mission is not None:
-            raise ValueError("Mission already exists with this ID")
-
-        new_mission = Mission(mission_id, name, description)
-        self.missions.append(new_mission)
